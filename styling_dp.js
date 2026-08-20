da0cc83795f9900fdf875cef6ac81906420a4195
dyunespace <jjung@bsgglobal.com>
2026-08-20 13:30:04 +0900

    chore: build timestamp log

--- styling_dp.js
+++ styling_dp.js
@@ -12,8 +12,8 @@
 	'use strict';
 
 	var TAG   = 'com-sap-sac-datepicker-glp-styling';
-	var BUILD = '20260820b';   // 캐시 확인용. meta_dp.json 의 ?v= 값과 같아야 한다.
-	console.log('[datepicker] styling build ' + BUILD);
+	var BUILD = '2026-08-20 13:29 KST';   // 배포할 때마다 갱신. 콘솔에서 반영 여부를 확인한다.
+	console.log('%c[datepicker] styling build ' + BUILD, 'color:#346187;font-weight:bold');
 
 	// 모드별 형식 선택지. value 가 실제 displayFormat 패턴.
 	var FORMATS = {
